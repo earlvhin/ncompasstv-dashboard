1ba6e1c11351df71200cb31d338f1744bcce66a8
jeromed <jeromed@n-compass.biz>
2021-04-09 18:58:28 +0800

    change env to preprod for testing

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -2,9 +2,7 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    //window["env"]["apiUrl"] = "https://apiecs.ourimage.biz/api/";
-    //window["env"]["socketUrl"] = "https://socketecs.ourimage.biz";
-    window["env"]["apiUrl"] = "http://3.212.225.229:30082/api/";
-    window["env"]["socketUrl"] = "http://3.212.225.229:30083";
+    window["env"]["apiUrl"] = "http://apiecs.ourimage.biz/api/";
+    window["env"]["socketUrl"] = "http://socketecs.ourimage.biz";
     window["env"]["debug"] = true;
   })(this);
