7a79e3f17644f4d43f884c01ecc2a3b48e2077ff
jeromed <jeromed@n-compass.biz>
2021-04-09 16:58:27 +0800

    nginx proxy config socket issue dashboard adjustment

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -2,7 +2,7 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "https://apiecs.ourimage.biz/api/";
-    window["env"]["socketUrl"] = "https://socketecs.ourimage.biz/";
+    window["env"]["apiUrl"] = "https://apiecs.ourimage.biz/api";
+    window["env"]["socketUrl"] = "https://socketecs.ourimage.biz";
     window["env"]["debug"] = true;
   })(this);
