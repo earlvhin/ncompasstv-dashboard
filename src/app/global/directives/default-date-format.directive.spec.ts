0887d7bf36265a9131fc3156b28810479a6b1d66
earlvhin <earlgabuat@gmail.com>
2021-05-04 15:49:56 +0800

    Align to Git

--- src/app/global/directives/default-date-format.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DefaultDateFormatDirective } from './default-date-format.directive';
-
-describe('DefaultDateFormatDirective', () => {
-	it('should create an instance', () => {
-		const directive = new DefaultDateFormatDirective();
-		expect(directive).toBeTruthy();
-	});
-});
