de2a0c5c7459e273c64e1c9e9eb70241845c6f94
earlvhin <earlgabuat@gmail.com>
2021-05-18 16:51:30 +0800

    Terser Issue Fix

--- src/assets/env/env.local.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["apiUrl"] = "http://127.0.0.1:82/api/";
-    window["env"]["debug"] = true;
-  })(this);
