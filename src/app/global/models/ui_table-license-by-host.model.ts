09758dca3d0c6c6338fa4c4be3b97c53320cf8a7
Earl Vhin Gabuat <earlgabuat@gmail.com>
2021-06-08 15:16:32 +0000

    Squashed commit of the following
    commit e20cb06a 
    Author: earlvhin <earlgabuat@gmail.com> 
    Date: Tue Jun 08 2021 09:13:07 GMT-0600 (Mountain Daylight Time) 
    
        Dealer Account > License Page > License Status Fix

--- src/app/global/models/ui_table-license-by-host.model.ts
+++ src/app/global/models/ui_table-license-by-host.model.ts
@@ -11,7 +11,7 @@ export class UI_TABLE_LICENSE_BY_HOST {
     connection_type: object;
     screen: object;
     create_date: object;
-	install_date: object;
+    install_date: object;
     contents_updated: object;
     status: object;
     pi_status: object;
@@ -22,9 +22,23 @@ export class UI_TABLE_LICENSE_BY_HOST {
     template?: object;
 
     constructor(
-        license_id: object, host_id: object, index: object, screenshot: object, license_key: object, alias: object, screen_type: object,
-        name: object, category: object, connection_type: object, screen: object, create_date: object, install_date: object, contents_updated: object, 
-        status: object, pi_status: object, template?: object
+        license_id: object, 
+        host_id: object, 
+        index: object, 
+        screenshot: object, 
+        license_key: object, 
+        alias: object, 
+        screen_type: object,
+        name: object, 
+        category: object, 
+        connection_type: object, 
+        screen: object, 
+        template: object,
+        create_date: object, 
+        install_date: object, 
+        contents_updated: object, 
+        status: object, 
+        pi_status: object
     ) {
         this.license_id = license_id;
         this.host_id = host_id;
@@ -39,7 +53,7 @@ export class UI_TABLE_LICENSE_BY_HOST {
         this.screen = screen;
         this.template = template;
         this.create_date = create_date;
-		this.install_date = install_date;
+        this.install_date = install_date;
         this.contents_updated = contents_updated;
         this.status = status;
         this.pi_status = pi_status;
