2b9234b5ea9ab4a9ee5af609c404d4f5885adc38
Mikoo Saguindang <mikoos@n-compass.biz>
2021-08-30 13:36:59 +0800

    fix(media-library): fix unclickable link
    Fix an issue where the link for generated feed content cannot be clicked

--- src/app/global/components_shared/media_components/thumbnail-card/thumbnail-card.component.ts
+++ src/app/global/components_shared/media_components/thumbnail-card/thumbnail-card.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { AuthService } from '../../../services/auth-service/auth.service';
-import { Observable, Subscription } from 'rxjs';
-import { environment } from '../../../../../environments/environment';
-import { UI_ROLE_DEFINITION } from 'src/app/global/models/ui_role-definition.model';
-import * as io from 'socket.io-client';
+import { environment } from 'src/environments/environment';
 import { MatDialog } from '@angular/material/dialog';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import * as io from 'socket.io-client';
+
+import { UI_ROLE_DEFINITION } from 'src/app/global/models';
+import { AuthService, ContentService } from 'src/app/global/services';
 import { ConfirmationModalComponent } from '../../../components_shared/page_components/confirmation-modal/confirmation-modal.component';
-import { ContentService } from '../../../services/content-service/content.service';
 
 @Component({
 	selector: 'app-thumbnail-card',
@@ -33,24 +34,23 @@ export class ThumbnailCardComponent implements OnInit {
 	@Output() deleted: EventEmitter<boolean> = new EventEmitter();
 	@Output() content_to_delete = new EventEmitter;
 
-	subscription: Subscription = new Subscription;
-	role: string;
 	route: string;
-	return_mes: string;
 	
-	_socket: any;
+	private return_mes: string;
+	private role: string;
+	
+	protected _socket: any;
+	protected _unsubscribe: Subject<void> = new Subject<void>();
 
 	constructor(
 		private _auth: AuthService,
 		private _dialog: MatDialog,
 		private _content: ContentService
-	) { 
-		
-	}
+	) { }
 
 	ngOnInit() {
 		this.role = Object.keys(UI_ROLE_DEFINITION).find(key => UI_ROLE_DEFINITION[key] === this._auth.current_user_value.role_id);
-		this.route = `/${this.role}/media-library/`
+		this.route = `/${this.role}/media-library/${this.content_id}`;
 
 		if (!this.disconnect_to_socket && this.filetype == 'webm' && this.is_converted == 0) {
 			this._socket = io(environment.socket_server, {
@@ -76,64 +76,53 @@ export class ThumbnailCardComponent implements OnInit {
 	}
 
 	ngOnDestroy() {
-		if (this._socket) {
-			this._socket.disconnect();
-		}
-	}
-	
-	removeFilenameHandle(e) {
-		// if (this.filetype !== 'feed') {
-		// 	return e.substring(e.indexOf('_') + 1);
-		// }
-		return e;
+		if (this._socket) this._socket.disconnect();
+		this._unsubscribe.next();
+		this._unsubscribe.complete();
 	}
 
-	deleteContentArray(e, y) {
-		if(e.checked) {
-			this.is_checked = true;
-		}
-		this.content_to_delete.emit({toadd: e.checked, id: y});
+	deleteContentArray(event: { checked: boolean }, content_id: string): void {
+		if (event.checked) this.is_checked = true;
+		this.content_to_delete.emit({ toadd: event.checked, id: content_id });
 	}
 
-	deleteMedia(e) {
+	deleteMedia(event): void {
 		this.warningModal('warning', 'Delete Content', 'Are you sure you want to delete this content?', this.return_mes, 'delete')
-		e.stopPropagation();
+		event.stopPropagation();
 	}
 	
-	deleteContentLogs() {
+	private deleteContentLogs() {
 		this.warningModal('warning', 'Delete Content Logs', 'Do you want to delete all the logs of this content','','delete-logs')
 	}
 
-	warningModal(status, message, data, return_msg, action) {
+	private warningModal(status: string, message: string, data: string, return_msg: string, action: string): void {
 		this._dialog.closeAll();
 		
-		let dialogRef = this._dialog.open(ConfirmationModalComponent, {
+		const dialogRef = this._dialog.open(ConfirmationModalComponent, {
 			width: '500px',
 			height: '350px',
-			data: {
-				status: status,
-				message: message,
-				data: data,
-				return_msg: return_msg,
-				action: action
-			}
-		})
+			data: { status, message, data, return_msg, action }
+		});
 
-		dialogRef.afterClosed().subscribe(result => {
-			var filter = [{
-				'contentid': this.content_id
-			}]
-			if(result == 'delete') {
-				this.subscription.add(
-					this._content.remove_content(filter).subscribe(
-						data => {
-							this.return_mes = data.message
-							this.deleted.emit(true)
-						}
-					)
-				)
-				
-			} else {}
-		} );
-	}
+		dialogRef.afterClosed()
+			.subscribe(
+				result => {
+
+					const filter = [{ 'contentid': this.content_id }];
+
+					if (result == 'delete') {
+						
+						this._content.remove_content(filter).pipe(takeUntil(this._unsubscribe))
+							.subscribe(
+								data => {
+									this.return_mes = data.message
+									this.deleted.emit(true)
+								},
+								error => console.log('Error removing content', error)
+							);
+						
+					}
+				} 
+			);
+		}
 }
