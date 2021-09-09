602349586409bc66d191857bfe1d4f921c603eba
vhin.gabuat-at-892444632159 <earlg@n-compass.biz>
2021-09-09 23:35:46 +0800

    Title, Filename Pipe

--- src/app/global/pipes/filename.pipe.ts
+++ src/app/global/pipes/filename.pipe.ts
@@ -11,7 +11,7 @@ export class FilenamePipe implements PipeTransform {
 		// const handle_removed = value.substring(value.indexOf('_') + 1);
 		// if (handle_removed.length > 15) {
 			this.slicedString = handle_removed.substr(0, 10);
-			return (this.slicedString + '...');
+			return (this.slicedString + `${handle_removed.length > 10 ? '...' : ''}`);
 		// } else {
 		// 	return handle_removed;
 		// }
