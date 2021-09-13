df1da0bfda22ab9f887301a6ff83806a156df975
Mikoo Saguindang <mikoos@n-compass.biz>
2021-09-13 15:05:42 +0800

    refactor(template service): refactor to use base service

--- src/app/global/services/template-service/template.service.ts
+++ src/app/global/services/template-service/template.service.ts
@@ -1,38 +1,33 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from '../auth-service/auth.service';
-import { environment } from '../../../../environments/environment';
+import { Observable } from 'rxjs';
+
+import { API_TEMPLATE, API_ZONE } from 'src/app/global/models';
+import { BaseService } from '../base.service';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
 	providedIn: 'root'
 })
 
-export class TemplateService {
+export class TemplateService extends BaseService {
 
-	token = JSON.parse(localStorage.getItem('tokens'));
 	onSelectZone = new EventEmitter<string>();
 
-	httpOptions = {
-		headers: new HttpHeaders(
-			{ 'Authorization': `Bearer ${this._auth.current_user_value.jwt.token}` }
-		)
-	};
-
-	constructor(
-		private _http: HttpClient,
-		private _auth: AuthService
-	) { }
-
-	get_templates() {
-		return this._http.get<any>(`${environment.base_uri}${environment.getters.api_get_templates}`, this.httpOptions);
+	get_templates(): Observable<API_TEMPLATE[]> {
+		return this.getRequest(environment.getters.api_get_templates);
 	}
 
-	get_template_by_id(id) {
-		return this._http.get<any>(`${environment.base_uri}${environment.getters.api_get_template_by_id}${id}`, this.httpOptions);
+	get_template_by_id(id: string): Observable<API_TEMPLATE[]> {
+		return this.getRequest(`${environment.getters.api_get_template_by_id}${id}`);
 	}
 
-	new_template(data) {
-		return this._http.post(`${environment.base_uri}${environment.create.api_new_template}`, data, this.httpOptions);
+	new_template(data: any) {
+		return this.postRequest(`${environment.create.api_new_template}`, data);
+	}
+
+	update_template_zone(data: API_ZONE) {
+		const url = `${environment.update.template_zone}/${data.templateZoneId}`;
+		return this.postRequest(url, data);
 	}
 
 }
