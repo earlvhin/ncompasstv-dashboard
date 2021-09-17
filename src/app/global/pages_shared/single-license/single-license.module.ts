976600277424830da27d303d6f3cb9e11bada16b
Nicole <nicoleannbustamante@gmail.com>
2021-09-17 13:48:22 +0800

    Implemented License -> Resource tab

--- src/app/global/pages_shared/single-license/single-license.module.ts
+++ src/app/global/pages_shared/single-license/single-license.module.ts
@@ -4,9 +4,10 @@ import { SingleLicenseComponent } from './single-license.component';
 import { GlobalModule } from '../../global.module';
 import { RouterModule } from '@angular/router';
 import { AnalyticsTabComponent } from './components/analytics-tab/analytics-tab.component';
+import { ResourceTabComponent } from './components/resource-tab/resource-tab.component';
 
 @NgModule({
-	declarations: [ SingleLicenseComponent, AnalyticsTabComponent ],
+	declarations: [ SingleLicenseComponent, AnalyticsTabComponent, ResourceTabComponent ],
 	imports: [
 		CommonModule,
 		GlobalModule,
