d834933c05837e5bda7830cc69967829b1fbb4f1
Nicole <nicoleannbustamante@gmail.com>
2021-10-12 11:52:19 +0800

    Added implementation for refresh token

--- src/app/global/middlewares/http-interceptor/http-error.interceptor.ts
+++ src/app/global/middlewares/http-interceptor/http-error.interceptor.ts
@@ -2,13 +2,19 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 import { Injectable } from '@angular/core';
 import { retry, catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth-service/auth.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
+    constructor(private authService: AuthService) {}
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request)
           .pipe(retry(4),
               catchError((error: HttpErrorResponse) => {
+                if([401, 403].includes(error.status) && this.authService.current_user_value) {
+                    //Refresh token
+                    this.authService.refresh_token();
+                }
                 console.log(`Error: ${error.status} - ${error.statusText} at ${error.url.substring(error.url.lastIndexOf('/') + 1)}`);
                 return throwError(error);
             })
