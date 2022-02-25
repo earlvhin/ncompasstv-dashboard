ac46a0b79028707c4fdda8160b1860d367906a52
Mikoo Saguindang <mikoos@n-compass.biz>
2022-02-25 18:51:33 +0800

    fix(licenses page export): round off duration in seconds to a whole number
    round off the seconds because some have a lot of decimal places
    
    NSB-3373

--- src/app/role-pages/administrator/pages/licenses/licenses.component.ts
+++ src/app/role-pages/administrator/pages/licenses/licenses.component.ts
@@ -766,9 +766,8 @@ export class LicensesComponent implements OnInit {
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
         let minutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds % 60;
-
-        return hours + "h " + minutes + "m " + seconds + "s "
+        let seconds = Math.floor(totalSeconds % 60);
+        return hours + "h " + minutes + "m " + seconds + "s ";
     }
 
 	exportTable(tab) {
