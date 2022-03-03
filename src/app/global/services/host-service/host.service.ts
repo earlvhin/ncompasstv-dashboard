23f657dc7780052a7f830d57ce7049142a6e0a98
renellac@n-compass.biz <KWcUkv1U5Qie>
2022-03-04 05:56:07 +0800

    NSB-3415 Fix Host Export

--- src/app/global/services/host-service/host.service.ts
+++ src/app/global/services/host-service/host.service.ts
@@ -135,7 +135,7 @@ export class HostService extends BaseService {
 
     get_host_by_dealer_id_with_sort(dealerId: string, page: number, search: string, sortColumn: string, sortOrder: string, pageSize = 15) {
 		const base = `${this.getters.api_get_host_by_id_optimized}`;
-		const params = this.setUrlParams({ dealerId, page, search, sortColumn, sortOrder, pageSize });
+		const params = this.setUrlParams({ dealerId, page, search, sortColumn, sortOrder, pageSize }, false, true);
 		const url = `${base}${params}`;
 		return this.getRequest(url);
 	}
