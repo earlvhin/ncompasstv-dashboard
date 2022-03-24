8ed3e5640b72915de64b64b36e11225d33f3aa0e
Mikoo Saguindang <mikoos@n-compass.biz>
2022-03-24 12:59:29 +0800

    fix(feature environment file): fix base uri and base uri old urls

--- src/environments/environment.feature.ts
+++ src/environments/environment.feature.ts
@@ -2,8 +2,8 @@ import { API_ENDPOINTS } from './api-endpoints';
 
 export const environment = {
 	production: true,
-	base_uri: 'https://development-amplify.dpwc8x700q1mo.amplifyapp.com/api/',
-	base_uri_old: 'https://development-amplify.dpwc8x700q1mo.amplifyapp.com/api/',
+	base_uri: 'https://devapi.n-compass.online/api/',
+	base_uri_old: 'https://devapi.n-compass.online/api/',
 	socket_server: 'https://nctvsocket.n-compass.online',
 	google_key: 'AIzaSyCtQeUg0lbyHkv-NwmlOKuR0AVYFOJ1VWY',
 	s3: 'https://n-compass-filestack.s3.amazonaws.com/',
