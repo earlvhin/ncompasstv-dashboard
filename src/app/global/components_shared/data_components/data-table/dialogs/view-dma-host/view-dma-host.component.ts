f303c12cb263d50937c84356fdd93b850cc84437
Mikoo Saguindang <mikoos@n-compass.biz>
2022-06-24 11:58:17 +0800

    refactor(view-dma-host component): remove log and implement on destroy hook

--- src/app/global/components_shared/data_components/data-table/dialogs/view-dma-host/view-dma-host.component.ts
+++ src/app/global/components_shared/data_components/data-table/dialogs/view-dma-host/view-dma-host.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
@@ -10,7 +10,7 @@ import { HostService } from 'src/app/global/services';
   templateUrl: './view-dma-host.component.html',
   styleUrls: ['./view-dma-host.component.scss']
 })
-export class ViewDmaHostComponent implements OnInit {
+export class ViewDmaHostComponent implements OnInit, OnDestroy {
 
     protected _unsubscribe = new Subject<void>();
     hosts_of_dma: any= [];
@@ -20,18 +20,21 @@ export class ViewDmaHostComponent implements OnInit {
         private _host: HostService,
     ) { }
 
-  ngOnInit() {
-    this._host.get_host_via_dma(this._dialog_data.status.data_to_fetch.rank, this._dialog_data.status.data_to_fetch.code, this._dialog_data.status.data_to_fetch.name).pipe(takeUntil(this._unsubscribe)).subscribe(
-		(data: any) => {
-            console.log("D", data)
-            this.hosts_of_dma = data.paging.entities;
-        }
-    )    
-  }
+	ngOnInit() {
+		this._host.get_host_via_dma(this._dialog_data.status.data_to_fetch.rank, this._dialog_data.status.data_to_fetch.code, this._dialog_data.status.data_to_fetch.name).pipe(takeUntil(this._unsubscribe)).subscribe(
+			(data: any) => {
+				this.hosts_of_dma = data.paging.entities;
+			}
+		)    
+	}
+
+	ngOnDestroy(): void {
+		this._unsubscribe.next();
+		this._unsubscribe.complete();
+	}
 
     getLink(page: string, id: string) {
         return `/administrator/${page}/${id}`;
     }
-
 
 }
