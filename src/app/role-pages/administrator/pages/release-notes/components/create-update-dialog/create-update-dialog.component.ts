74626f9fcf2603da3c78866ab2bdf92777e43f4b
renellac@n-compass.biz <KWcUkv1U5Qie>
2022-09-07 09:34:37 +0800

    livestream updates

--- src/app/role-pages/administrator/pages/release-notes/components/create-update-dialog/create-update-dialog.component.ts
+++ src/app/role-pages/administrator/pages/release-notes/components/create-update-dialog/create-update-dialog.component.ts
@@ -7,7 +7,6 @@ import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 import { API_RELEASE_NOTE, UI_CONFIRMATION_MODAL } from 'src/app/global/models';
 import { AuthService, ConfirmationDialogService, ReleaseNotesService } from 'src/app/global/services';
-import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 
 @Component({
@@ -27,53 +26,6 @@ export class CreateUpdateDialogComponent implements OnInit, OnDestroy {
 	textPreview = '';
 	title = 'Create Release Notes';
 	protected _unsubscribe = new Subject<void>();
-
-    editorConfig: AngularEditorConfig = {
-        editable: true,
-          spellcheck: true,
-          height: 'auto',
-          minHeight: '0',
-          maxHeight: 'auto',
-          width: 'auto',
-          minWidth: '0',
-          translate: 'yes',
-          enableToolbar: true,
-          showToolbar: true,
-          placeholder: 'Enter text here...',
-          defaultParagraphSeparator: '',
-          defaultFontName: '',
-          defaultFontSize: '',
-          fonts: [
-            {class: 'arial', name: 'Arial'},
-            {class: 'times-new-roman', name: 'Times New Roman'},
-            {class: 'calibri', name: 'Calibri'},
-            {class: 'comic-sans-ms', name: 'Comic Sans MS'}
-          ],
-          customClasses: [
-          {
-            name: 'quote',
-            class: 'quote',
-          },
-          {
-            name: 'redText',
-            class: 'redText'
-          },
-          {
-            name: 'titleText',
-            class: 'titleText',
-            tag: 'h1',
-          },
-        ],
-        uploadUrl: 'v1/image',
-        // upload: (file: File) => { ... }
-        // uploadWithCredentials: false,
-        // sanitize: true,
-        // toolbarPosition: 'top',
-        // toolbarHiddenButtons: [
-        //   ['bold', 'italic'],
-        //   ['fontSize']
-        // ]
-    }
 	
 	constructor(
 		private _alert: ConfirmationDialogService,
