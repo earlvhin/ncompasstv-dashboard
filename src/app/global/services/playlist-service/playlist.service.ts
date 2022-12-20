241ffdd8eca51eb2982da52b42023f52d6697718
Mikoo Saguindang <mikoos@n-compass.biz>
2022-12-20 21:15:26 +0800

    fix(playlist-content-panel component): fix playlist content delete
    fix an issue where deleting playlist contents does not work
    
    NSB-3836

--- src/app/global/services/playlist-service/playlist.service.ts
+++ src/app/global/services/playlist-service/playlist.service.ts
@@ -1,12 +1,11 @@
 import { Injectable, EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 
+import { API_BLOCKLIST_CONTENT, API_SWAP_CONTENT_RESPONSE } from 'src/app/global/models';
 import { AuthService } from 'src/app/global/services/auth-service/auth.service';
-import { environment } from 'src/environments/environment';
-import { API_BLOCKLIST_CONTENT, API_SINGLE_PLAYLIST, API_SWAP_CONTENT_RESPONSE } from 'src/app/global/models';
-import { Observable } from 'rxjs';
 import { BaseService } from '../base.service';
 
 @Injectable({
@@ -15,8 +14,8 @@ import { BaseService } from '../base.service';
 export class PlaylistService extends BaseService {
 	onBlacklistDataReady = new EventEmitter<API_BLOCKLIST_CONTENT[]>();
 	onPushPlaylistUpdateToAllLicenses = new EventEmitter<null>();
-	
-    token = JSON.parse(localStorage.getItem('tokens'));
+
+	token = JSON.parse(localStorage.getItem('tokens'));
 
 	httpOptions = {
 		headers: new HttpHeaders({ 'Content-Type': 'application/json', credentials: 'include', Accept: 'application/json' })
@@ -37,116 +36,118 @@ export class PlaylistService extends BaseService {
 
 	blocklist_content(data) {
 		return this.postRequest(this.updaters.api_blocklist_content, data);
-    }
+	}
 
 	blacklist_cloned_content(playlistContentId, playlistId, contentId) {
-        return this.postRequest(`${this.updaters.api_blacklist_cloned_content}?playlistContentId=${playlistContentId}&playlistId=${playlistId}&contentId=${contentId}`, null);
+		return this.postRequest(
+			`${this.updaters.api_blacklist_cloned_content}?playlistContentId=${playlistContentId}&playlistId=${playlistId}&contentId=${contentId}`,
+			null
+		);
 	}
 
 	export_playlist(id) {
-        return this.getRequest(`${this.getters.export_content_playlist}${id}`);
+		return this.getRequest(`${this.getters.export_content_playlist}${id}`);
 	}
 
 	get_blacklisted_by_id(playlist_content_id: string) {
-        return this.getRequest(`${this.getters.api_get_blacklisted_by_id}${playlist_content_id}`);
+		return this.getRequest(`${this.getters.api_get_blacklisted_by_id}${playlist_content_id}`);
 	}
 
 	get_playlists(page, key) {
-        return this.getRequest(`${this.getters.api_get_playlist}` + '?page=' + `${page}` + '&search=' + `${key}`);
+		return this.getRequest(`${this.getters.api_get_playlist}` + '?page=' + `${page}` + '&search=' + `${key}`);
 	}
 
 	get_all_playlists(page, key, column?, order?) {
-        const base = `${this.getters.api_get_all_playlist}`;
+		const base = `${this.getters.api_get_all_playlist}`;
 		const params = this.setUrlParams({ page, search: key, sortColumn: column, sortOrder: order }, false, true);
 		const url = `${base}${params}`;
 		return this.getRequest(url);
 	}
 
 	get_playlist_by_content_id(content_id: string) {
-        return this.getRequest(`${this.getters.api_get_playlist_by_content}${content_id}`).pipe(map((i) => i.playlists));
+		return this.getRequest(`${this.getters.api_get_playlist_by_content}${content_id}`).pipe(map((i) => i.playlists));
 	}
 
 	get_playlists_total() {
-        return this.getRequest(`${this.getters.api_get_playlist_total}`);
+		return this.getRequest(`${this.getters.api_get_playlist_total}`);
 	}
 
 	get_playlists_total_by_dealer(id) {
-        return this.getRequest(`${this.getters.api_get_playlist_total_by_dealer}${id}`);
+		return this.getRequest(`${this.getters.api_get_playlist_total_by_dealer}${id}`);
 	}
 
 	get_playlist_by_dealer_id(id) {
-        return this.getRequest(`${this.getters.api_get_playlist_by_dealer_id}${id}`);
+		return this.getRequest(`${this.getters.api_get_playlist_by_dealer_id}${id}`);
 	}
 
 	get_playlist_by_dealer_id_table(page, id, key) {
-        const base = `${this.getters.api_get_playlist_by_dealer_id_table}`;
+		const base = `${this.getters.api_get_playlist_by_dealer_id_table}`;
 		const params = this.setUrlParams({ page, dealerid: id, search: key }, false, true);
 		const url = `${base}${params}`;
 		return this.getRequest(url);
 	}
 
 	get_playlist_by_dealer_id_v2(id) {
-        return this.getRequest(`${this.getters.api_get_playlist_by_dealer_id_table}?dealerId=${id}&pageSize=0`);
+		return this.getRequest(`${this.getters.api_get_playlist_by_dealer_id_table}?dealerId=${id}&pageSize=0`);
 	}
 
 	get_playlist_by_id(id) {
-        return this.getRequest(`${this.getters.api_get_playlists_by_id}${id}`);
+		return this.getRequest(`${this.getters.api_get_playlists_by_id}${id}`);
 	}
 
 	get_screens_of_playlist(id) {
-        return this.getRequest(`${this.getters.api_get_screens_of_playlist}${id}`);
+		return this.getRequest(`${this.getters.api_get_screens_of_playlist}${id}`);
 	}
 
 	create_playlist(data) {
-        return this.postRequest(this.creators.api_new_playlist, data);
+		return this.postRequest(this.creators.api_new_playlist, data);
 	}
 
 	clone_playlist(data) {
-        return this.postRequest(this.creators.api_clone_playlist, data);
+		return this.postRequest(this.creators.api_clone_playlist, data);
 	}
 
 	bulk_whitelist(data) {
-        return this.postRequest(this.deleters.api_bulk_remove_in_blacklist, data);
+		return this.postRequest(this.deleters.api_bulk_remove_in_blacklist, data);
 	}
 
 	remove_playlist(id, force) {
-        const url = `${this.deleters.api_remove_playlist}${id}&force=${force}`;
-        return this.postRequest(url,{});
+		const url = `${this.deleters.api_remove_playlist}${id}&force=${force}`;
+		return this.postRequest(url, {});
 	}
 
 	remove_playlist_content(playlist, content) {
-        const url = `${this.deleters.api_remove_playlist_content}?playlistId=${playlist}&playlistContentId=${content}`;
-        return this.postRequest(url,{});
+		const url = `${this.deleters.api_remove_playlist_content}?playlistId=${playlist}&playlistContentId=${content}`;
+		return this.postRequest(url, {});
 	}
 
-	remove_playlist_contents(playlist, contents) {
-        const url = `${this.deleters.api_remove_playlist_contents}?playlistId=${playlist}`;
-        return this.postRequest(url,{});
+	remove_playlist_contents(playlistId: string, contentIdsToDelete: string[]) {
+		const url = `${this.deleters.api_remove_playlist_contents}?playlistId=${playlistId}`;
+		return this.postRequest(url, contentIdsToDelete);
 	}
 
 	remove_in_blocklist(data) {
 		const requestOptions: Object = {
-			/* other options here */
 			responseType: 'text'
 		};
-        return this.postRequest(this.deleters.api_remove_in_blacklist,data, requestOptions);
+		return this.postRequest(this.deleters.api_remove_in_blacklist, data, requestOptions);
 	}
 
 	log_content_history(data) {
-        return this.postRequest(this.creators.api_new_content_history_log, data);
+		return this.postRequest(this.creators.api_new_content_history_log, data);
 	}
 
-	swap_playlist_content(data: { playlistContentId: string, contentId: string }): Observable<API_SWAP_CONTENT_RESPONSE> {
+	swap_playlist_content(data: { playlistContentId: string; contentId: string }): Observable<API_SWAP_CONTENT_RESPONSE> {
 		const { playlistContentId, contentId } = data;
 		const url = `${this.updaters.swap_playlist_content}?playlistContentId=${playlistContentId}&contentId=${contentId}`;
 		return this.postRequest(url, {});
 	}
 
 	update_playlist_info(playlist_data) {
-        return this.postRequest(this.updaters.api_update_playlist_info, playlist_data);
+		return this.postRequest(this.updaters.api_update_playlist_info, playlist_data);
 	}
 
 	update_playlist_contents(playlist_data) {
-        return this.postRequest(this.updaters.api_update_playlist_content, playlist_data);
+		return this.postRequest(this.updaters.api_update_playlist_content, playlist_data);
 	}
 }
