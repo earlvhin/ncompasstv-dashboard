ad549ac0615de391037f360798f55d1a61313c3e
earlvhin <earlgabuat@gmail.com>
2023-03-22 21:49:14 +0800

    set overrideUrl to true

--- src/app/global/services/fastedge-service/fastedge.service.ts
+++ src/app/global/services/fastedge-service/fastedge.service.ts
@@ -7,6 +7,6 @@ import { BaseService } from '../base.service';
 })
 export class FastEdgeService extends BaseService {
 	get_google_business_profile(data) {
-		return this.getRequest(`${environment.fastedge}${data}`, { 'x-api-key': 'kIwFkm6nVF5qYvAQfYKjB6h516yA918w5m1COWZA' }, true, false, true);
+		return this.getRequest(`${environment.fastedge}${data}`, { 'x-api-key': 'kIwFkm6nVF5qYvAQfYKjB6h516yA918w5m1COWZA' }, false, true, true);
 	}
 }
