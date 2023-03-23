9579281da1a3600eac356639bcc859023624ab63
renellac@n-compass.biz <KWcUkv1U5Qie>
2023-03-24 07:01:22 +0800

    Google new API dev implemented

--- src/app/global/services/fastedge-service/fastedge.service.ts
+++ src/app/global/services/fastedge-service/fastedge.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { BaseService } from '../base.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class FastEdgeService extends BaseService {
 	get_google_business_profile(data) {
-		return this.getRequest(`${environment.fastedge}${data}`, { 'x-api-key': 'kIwFkm6nVF5qYvAQfYKjB6h516yA918w5m1COWZA' }, true, true);
+        let httpOptions = {
+            headers: new HttpHeaders({ 'x-api-key': 'kIwFkm6nVF5qYvAQfYKjB6h516yA918w5m1COWZA' })
+        };
+		return this.getRequest(`${environment.fastedge}${data}`, httpOptions, true, true);
 	}
 }
