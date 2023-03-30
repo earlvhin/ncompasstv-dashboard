d4d118af6eeef8a6c9276bf4399a939d658fd985
renellac@n-compass.biz <KWcUkv1U5Qie>
2023-03-30 19:24:08 +0800

    Update error in build

--- src/environments/environment.staging-new.ts
+++ src/environments/environment.staging-new.ts
@@ -7,6 +7,7 @@ export const environment = {
 	socket_server: 'https://stg-socket.n-compass.online/',
 	google_key: 'AIzaSyCtQeUg0lbyHkv-NwmlOKuR0AVYFOJ1VWY',
 	s3: 'https://n-compass-filestack.s3.amazonaws.com/',
+    fastedge: 'https://fastedgeapistg.n-compass.online/nc/googleapi/searchplaces?place=',
 	auth: API_ENDPOINTS.auth,
 	getters: API_ENDPOINTS.getters,
 	create: API_ENDPOINTS.create,
