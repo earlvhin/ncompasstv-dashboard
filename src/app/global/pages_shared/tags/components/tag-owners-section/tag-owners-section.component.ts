e60376e87f8f5bc26947fb431b9464a2f2c4e31a
jeremicah <jlicup@n-compass.biz>
2023-07-03 03:01:50 +0000

    Squashed commit of the following
    commit 1b521a1f 
    Author: jeremicah <jlicup@n-compass.online> 
    Date: Fri Jun 30 2023 22:09:39 GMT+0800 (Singapore Standard Time) 
    
        sort and remove the owner ID
    
    
    commit dd044b1f 
    Author: jeremicah <jlicup@n-compass.online> 
    Date: Mon Jun 26 2023 22:09:55 GMT+0800 (Singapore Standard Time) 
    
        added button to export csv file in tags

--- src/app/global/pages_shared/tags/components/tag-owners-section/tag-owners-section.component.ts
+++ src/app/global/pages_shared/tags/components/tag-owners-section/tag-owners-section.component.ts
@@ -3,6 +3,8 @@ import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
 import { debounceTime, map, takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { saveAs } from 'file-saver';
+import { Workbook } from 'exceljs';
 
 import { PAGING, TAG, TAG_OWNER, TAG_TYPE, UI_ROLE_DEFINITION_TEXT } from 'src/app/global/models';
 import { TagService, AuthService } from 'src/app/global/services';
@@ -31,7 +33,20 @@ export class TagOwnersSectionComponent implements OnInit, OnDestroy {
 	pagingData: PAGING;
 	searchFormControl = new FormControl(null, Validators.minLength(3));
 	selectedTag: TAG;
+	tags_to_export: any[] = [];
+	workbook: any;
+	workbook_generation: boolean = false;
+	worksheet: any;
 	protected _unsubscribe: Subject<void> = new Subject<void>();
+
+	tags_table_column = [
+		{ name: '#', sortable: false, no_export: true },
+		{ name: 'Assignee', key: 'displayName' },
+		{ name: 'Owner / Dealer Alias', key: 'owner' },
+		{ name: 'Tag Type', key: 'tagTypeName' },
+		{ name: 'License Key', key: 'licenseKey' },
+		{ name: 'Tags', key: 'tags' }
+	];
 
 	constructor(private _dialog: MatDialog, private _tag: TagService, private _auth: AuthService) {}
 
@@ -105,6 +120,101 @@ export class TagOwnersSectionComponent implements OnInit, OnDestroy {
 			this._tag.onRefreshTagOwnersTable.next();
 			this._tag.onRefreshTagsTable.next();
 		});
+	}
+
+	exportTable() {
+		this.workbook_generation = true;
+		const header = [];
+		this.workbook = new Workbook();
+		this.workbook.creator = 'NCompass TV';
+		this.workbook.useStyles = true;
+		this.workbook.created = new Date();
+		this.worksheet = this.workbook.addWorksheet('Tags');
+		Object.keys(this.tags_table_column).forEach((key) => {
+			if (this.tags_table_column[key].name && !this.tags_table_column[key].no_export) {
+				header.push({
+					header: this.tags_table_column[key].name,
+					key: this.tags_table_column[key].key,
+					width: 30,
+					style: { font: { name: 'Arial', bold: true } }
+				});
+			}
+		});
+
+		this.worksheet.columns = header;
+		this.getDataForExport();
+	}
+
+	getDataForExport(keyword: string = null, page = 1, pageSize = 0): void {
+		const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+
+		if (this.searchFormControl.value) keyword = this.searchFormControl.value;
+
+		this._tag
+			.getAllOwnerAndTags({ keyword, page, pageSize }, this._isDealer())
+			.pipe(takeUntil(this._unsubscribe))
+			.subscribe((response) => {
+				const tags = response.paging.entities as TAG[];
+
+				if (tags.length <= 0) {
+					this.tags_to_export = [];
+					return;
+				} else {
+					this.tags_to_export = [...response.paging.entities];
+				}
+
+				this.tags_to_export.sort((a, b) => {
+					const assigneeA = a.displayName.toLowerCase();
+					const assigneeB = b.displayName.toLowerCase();
+
+					if (assigneeA < assigneeB) {
+						return -1;
+					} else if (assigneeA > assigneeB) {
+						return 1;
+					} else {
+						return 0;
+					}
+				});
+
+				this.tags_to_export.sort((a, b) => {
+					const tagTypeNameA = a.tagTypeName.toLowerCase();
+					const tagTypeNameB = b.tagTypeName.toLowerCase();
+
+					if (tagTypeNameA < tagTypeNameB) {
+						return -1;
+					} else if (tagTypeNameA > tagTypeNameB) {
+						return 1;
+					} else {
+						return 0;
+					}
+				});
+
+				this.tags_to_export.forEach((item) => {
+					const tagNames = item.tags.map((tag) => tag.name).join(', ');
+					const ownerAlias = item.owner.alias || item.owner.dealerIdAlias;
+					const row = {
+						licenseKey: item.owner.licenseKey,
+						displayName: item.displayName,
+						tagTypeName: item.tagTypeName,
+						tags: tagNames,
+						owner: ownerAlias
+					};
+					this.worksheet.addRow(row).font = { bold: false };
+				});
+
+				let rowIndex = 1;
+				for (rowIndex; rowIndex <= this.worksheet.rowCount; rowIndex++) {
+					this.worksheet.getRow(rowIndex).alignment = { vertical: 'middle', horizontal: 'center', wrapText: true };
+				}
+
+				this.workbook.xlsx.writeBuffer().then((file: any) => {
+					const blob = new Blob([file], { type: EXCEL_TYPE });
+					const filename = 'Tags' + '.xlsx';
+					saveAs(blob, filename);
+				});
+
+				this.workbook_generation = false;
+			});
 	}
 
 	private clearSelectedTag(): void {
