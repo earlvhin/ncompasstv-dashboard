e0d95d596a517c2633df8a0775263bb31b8bf085
earlvhin <earlgabuat@gmail.com>
2023-12-14 17:06:04 +0800

    feat(dynamic-autocomplete): add search trigger

--- src/app/global/components_shared/data_components/autocomplete/autocomplete.component.ts
+++ src/app/global/components_shared/data_components/autocomplete/autocomplete.component.ts
@@ -2,43 +2,47 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
-import { UI_AUTOCOMPLETE } from 'src/app/global/models';
+import { UI_AUTOCOMPLETE, UI_AUTOCOMPLETE_DATA } from 'src/app/global/models';
 
 @Component({
-    selector: 'app-autocomplete',
-    templateUrl: './autocomplete.component.html',
-    styleUrls: ['./autocomplete.component.scss']
+	selector: 'app-autocomplete',
+	templateUrl: './autocomplete.component.html',
+	styleUrls: ['./autocomplete.component.scss']
 })
 export class AutocompleteComponent implements OnInit {
-    @Input() field_data: UI_AUTOCOMPLETE = {
-        label: 'Label',
-        placeholder: 'Type anything',
-        data: []
-    };
-    @Output() value_selected: EventEmitter<{ id: string; value: string }> = new EventEmitter();
+	@Input() field_data: UI_AUTOCOMPLETE = {
+		label: 'Label',
+		placeholder: 'Type anything',
+		data: []
+	};
 
-    autoCompleteControl = new FormControl();
-    filteredOptions!: Observable<any[]>;
+	@Output() value_selected: EventEmitter<{ id: string; value: string }> = new EventEmitter();
+	@Output() trigger_search: EventEmitter<string> = new EventEmitter();
 
-    constructor() {}
+	autoCompleteControl = new FormControl();
+	filteredOptions!: Observable<any[]>;
 
-    ngOnInit() {
-        this.filteredOptions = this.autoCompleteControl.valueChanges.pipe(
-            startWith(''),
-            map((keyword) => this._filter(keyword))
-        );
-    }
+	constructor() {}
 
-    displayOption(option: any): string {
-        return option ? option.value : '';
-    }
+	ngOnInit() {
+		this.filteredOptions = this.autoCompleteControl.valueChanges.pipe(
+			startWith(''),
+			map((keyword) => this._filter(keyword))
+		);
+	}
 
-    private _filter(keyword: any) {
-        const filterValue = keyword.hasOwnProperty('value') ? keyword.value.toLowerCase() : keyword.toLowerCase();
-        return this.field_data.data.filter((option) => option.value.toLowerCase().includes(filterValue));
-    }
+	displayOption(option: any): string {
+		return option ? option.value : '';
+	}
 
-    valueSelected(e) {
-        this.value_selected.emit(e.option.value.id);
-    }
+	private _filter(keyword: any) {
+		const filterValue = keyword.hasOwnProperty('value') ? keyword.value.toLowerCase() : keyword.toLowerCase();
+		const filterResult = this.field_data.data.filter((option) => option.value.toLowerCase().includes(filterValue));
+		if (!filterResult.length && this.field_data.allowSearchTrigger) this.trigger_search.emit(keyword);
+		return filterResult;
+	}
+
+	valueSelected(e) {
+		this.value_selected.emit(e.option.value.id);
+	}
 }
