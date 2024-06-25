e2a56e9acb9c76871f92a8db338583ea62e60bd2
Fratz Antigua <fratz.antigua-at-892444632159>
2024-06-25 13:55:14 +0800

    feat(nsp-210): initial changes

--- src/app/global/components_shared/license_components/target-license-modal/target-license.component.ts
+++ src/app/global/components_shared/license_components/target-license-modal/target-license.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatSelect } from '@angular/material';
 // import { LicenseService } from '../../../services/license-service/license.service';
 import { API_CONTENT, API_BLOCKLIST_CONTENT, CREDITS, PLAYLIST_CHANGES, API_LICENSE } from 'src/app/global/models';
 import { Subject } from 'rxjs';
-import { API_DEALER } from 'src/app/global/models/api_dealer.model';
+import { API_DEALER, API_HOST } from 'src/app/global/models';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,28 +12,20 @@ import { Subscription } from 'rxjs';
     templateUrl: './target-license.component.html',
     styleUrls: ['./target-license.component.scss'],
 })
-export class TargetLicenseModal implements OnInit {
-    generate_license_form: FormGroup;
-    dealer_id: string;
-    is_submitted: boolean;
-    license_generated: boolean;
-    dealerHasValue: boolean;
-    invalid_form: boolean = true;
-    subscription = new Subscription();
-    host_license: any;
-    toggle_event: Subject<void> = new Subject<void>();
-    total_licenses = 0;
-    total_whitelist = 0;
-    is_child_frequency = false;
-    form = this._form.group({
-        selectedOwners: [[], Validators.required],
-        selectedTags: [[], Validators.required],
-    });
-    selected_data: any;
-    selectedDealersControl = this.form.get('selectedOwners');
+export class TargetLicenseModal implements OnInit, OnDestroy {
+    @ViewChild('dealerMultiSelect', { static: false }) dealerMultiSelect: MatSelect;
+    dealerSelection = this._formBuilder.group({ selectedDealers: [[], Validators.required] });
+    dealerFilterControl = new FormControl(null);
+    isLoadingHosts = true;
+    isSearching = false;
+    selectedDealers: API_DEALER[];
+    selectedHosts: API_HOST[];
+    selectedDealersControl = this.dealerSelection.get('selectedDealers');
+
+    
 
     constructor(
-        private _form: FormBuilder,
+        private _formBuilder: FormBuilder,
     ) {}
 
     ngOnInit() {
@@ -42,17 +35,29 @@ export class TargetLicenseModal implements OnInit {
         });
     }
 
+    ngOnDestroy() {
+        this._unsubscribe.next();
+        this._unsubscribe.complete();
+    }
+
     // Convenience getter for easy access to form fields
     get formControls() {
         return this.generate_license_form.controls;
     }
-    
 
-    public setAssignedTo(dealer: any): void {
-        this.dealerHasValue = false;
-        if (dealer != null) {
-            this.formControls.dealer.setValue(dealer.id);
-            this.dealerHasValue = true;
-        }
+    onRemoveDealer(index: number) {
+        this.selectedDealersControl.value.splice(index, 1);
+        this.dealerMultiSelect.compareWith = (a, b) => a && b && a.dealerId === b.dealerId;
+        this.onSelectDealer();
+    }
+    
+    onClearSelection() {
+        this.selectedDealersControl.value.length = 0;
+        this.dealerMultiSelect.compareWith = (a, b) => a && b && a.dealerId === b.dealerId;
+        this.selectedDealers = [];
+        this.unfilteredDealers = [];
+        this.unfilteredLicenses = [];
+        this.unfilteredHosts = [];
+        this.mapMarkers = [];
     }
 }
