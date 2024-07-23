dccef856fd08976a98daa00b6cf7d500df599348
efraim <efraimg@n-compass.biz>
2024-07-23 05:11:30 +0000

    Squashed commit of the following
    commit fb962d53 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Tue Jul 23 2024 10:45:57 GMT+0800 (Philippine Standard Time) 
    
        feat(nsp-418): updated env to dev
    
    
    commit 8f7b8acf 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Fri Jul 19 2024 11:30:48 GMT+0800 (Philippine Standard Time) 
    
        feat(nsp-418): implemented fix on edit form
    
    
    commit e9330a6f 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Thu Jul 18 2024 13:33:56 GMT+0800 (Philippine Standard Time) 
    
        update to one-liner
    
    
    commit 942a9358 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Thu Jul 18 2024 13:23:24 GMT+0800 (Philippine Standard Time) 
    
        removed consolelogs
    
    
    commit f0bb861d 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Thu Jul 18 2024 10:48:03 GMT+0800 (Philippine Standard Time) 
    
        env uri
    
    
    commit 4163c480 
    Author: efraim <efraimg@n-compass.biz> 
    Date: Wed Jul 17 2024 17:31:58 GMT+0800 (Philippine Standard Time) 
    
        feat(nsp-418): autocomplete dealer on weather feed

--- src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
+++ src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
@@ -1,10 +1,11 @@
 import { EventEmitter, HostListener, Output } from '@angular/core';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
-import { API_FEED_TYPES, API_GENERATED_FEED, GenerateSlideFeed } from 'src/app/global/models';
+import { API_FEED_TYPES, API_GENERATED_FEED, GenerateSlideFeed, UI_AUTOCOMPLETE_DATA } from 'src/app/global/models';
 import { AuthService, FeedService } from 'src/app/global/services';
 
 @Component({
@@ -12,7 +13,7 @@ import { AuthService, FeedService } from 'src/app/global/services';
     templateUrl: './feed-info.component.html',
     styleUrls: ['./feed-info.component.scss'],
 })
-export class FeedInfoComponent implements OnInit {
+export class FeedInfoComponent implements OnInit, OnDestroy {
     @Input() dealers: { dealerId: string; businessName: string }[];
     @Input() editing: boolean = false;
     @Input() fetched_feed: API_GENERATED_FEED;
@@ -21,11 +22,16 @@ export class FeedInfoComponent implements OnInit {
     @Output() feed_info = new EventEmitter();
     @Output() formChanges: EventEmitter<boolean> = new EventEmitter<boolean>();
 
+    autoCompleteSelectedDealer: UI_AUTOCOMPLETE_DATA[] = [];
+    selectedDealer: { dealerId: string; businessName: string };
+    dealerHasValue = false;
     existing: any;
     filtered_options: Observable<{ dealerId: string; businessName: string }[]>;
     generated_feed: GenerateSlideFeed;
     hasUnsavedChanges: boolean = false;
     isDealer = this._auth.current_role === 'dealer' || this._auth.current_role === 'sub-dealer';
+    editingAsDealer = false;
+    editHasValue = true;
     new_feed_form: FormGroup;
     selected_dealer: string;
     private formSubscription: Subscription;
@@ -34,21 +40,26 @@ export class FeedInfoComponent implements OnInit {
         private _auth: AuthService,
         private _form: FormBuilder,
         private _feed: FeedService,
+        private _location: Location,
     ) {}
 
     ngOnInit() {
+        if (this.editing && this.fetched_feed) this.initialSelectedDealer();
         this.prepareFeedInfoForm();
 
         this.formSubscription = this.new_feed_form.valueChanges.subscribe((f) => {
-            this.hasUnsavedChanges =
-                f.feed_title || f.feed_type || f.description || (f.assign_to !== '' && !this.isDealer);
+            this.hasUnsavedChanges = this.checkForUnsavedChanges(f);
             this.formChanges.emit(this.hasUnsavedChanges);
             this.updateHasUnsavedChanges(this.hasUnsavedChanges);
         });
+
+        if (this.isDealer) this.editingAsDealer = true;
+
+        window.addEventListener('beforeunload', this.unloadNotification.bind(this));
     }
 
     ngOnDestroy() {
-        window.removeEventListener('beforeunload', this.unloadNotification);
+        window.removeEventListener('beforeunload', this.unloadNotification.bind(this));
         this.formSubscription.unsubscribe();
     }
 
@@ -59,30 +70,46 @@ export class FeedInfoComponent implements OnInit {
         }
     }
 
-    updateHasUnsavedChanges(value: boolean) {
-        this._feed.setInputChanges(value);
-    }
-
-    /** Structure Feed Information and Pass */
-    structureFeedInfo() {
-        if (!this.editing) {
-            this.feed_info.emit(this.new_feed_form.value);
-        } else {
-            /** Temp workaround (For some reason assign_to_id field value is not reflecting ) */
-            this.feed_info.emit({
-                feed_title: this.new_feed_form.controls.feed_title.value,
-                description: this.new_feed_form.controls.description.value,
-                feed_type: this.fetched_feed.feedType.feedTypeId,
-                assign_to_id: this.fetched_feed.dealerId,
+    private initialSelectedDealer(): void {
+        if (this.fetched_feed && this.fetched_feed.dealer) {
+            this.autoCompleteSelectedDealer.push({
+                id: this.fetched_feed.dealer.dealerId,
+                value: this.fetched_feed.dealer.businessName,
             });
         }
     }
 
-    /**
-     * Filter Method for the Angular Material Autocomplete
-     * @param {string} value The entered phrase in the field
-     * @returns {dealerId: string, businessName: string} Array of filtered results
-     */
+    updateHasUnsavedChanges(value: boolean) {
+        this._feed.setInputChanges(value);
+    }
+
+    structureFeedInfo() {
+        const feedData = this.editing ? this.getEditingFeedData() : this.new_feed_form.value;
+        this.feed_info.emit(feedData);
+    }
+
+    private getEditingFeedData() {
+        return {
+            feed_title: this.new_feed_form.controls.feed_title.value,
+            description: this.new_feed_form.controls.description.value,
+            feed_type: this.fetched_feed && this.fetched_feed.feedType ? this.fetched_feed.feedType.feedTypeId : null,
+            assign_to_id: this.selectedDealer
+                ? this.selectedDealer.dealerId
+                : this.fetched_feed && this.fetched_feed.dealer
+                  ? this.fetched_feed.dealer.dealerId
+                  : null,
+        };
+    }
+
+    private checkForUnsavedChanges(formValue: any): boolean {
+        return (
+            formValue.feed_title ||
+            formValue.feed_type ||
+            formValue.description ||
+            (formValue.assign_to !== '' && !this.isDealer)
+        );
+    }
+
     private filter(value: string): { dealerId: string; businessName: string }[] {
         const filter_value = value.toLowerCase();
         const filtered_result = this.dealers
@@ -97,46 +124,71 @@ export class FeedInfoComponent implements OnInit {
         return filtered_result;
     }
 
-    /** Build Feed Information Form with fields of feed_title, description, assign_to */
     private prepareFeedInfoForm(): void {
-        let config: { [key: string]: any };
-        const feed = this.fetched_feed;
+        const config = this.getFormConfig();
+        this.new_feed_form = this._form.group(config);
+        this.initializeAutocomplete();
+    }
 
-        config = {
+    private getFormConfig(): { [key: string]: any } {
+        const baseConfig = {
             feed_title: ['', Validators.required],
             description: [''],
             feed_type: ['', Validators.required],
-            assign_to: [this.is_dealer ? this.dealers[0].businessName : ''],
-            assign_to_id: [this.is_dealer ? this.dealers[0].dealerId : ''],
+            assign_to: [this.dealerHasValue ? this.selectedDealer.businessName : '', Validators.required],
+            assign_to_id: [this.dealerHasValue ? this.selectedDealer.dealerId : '', Validators.required],
         };
 
         if (this.editing) {
-            config['feed_title'] = [feed.feedTitle, Validators.required];
-            config['description'] = [feed.description];
-            config['feed_type'] = [{ value: feed.feedType.feedTypeId, disabled: true }, Validators.required];
-            config['assign_to'] = [{ value: null, disabled: true }];
-            config['assign_to_id'] = [{ value: null, disabled: true }];
-
-            if (feed.dealerId) {
-                config['assign_to'] = [{ value: feed.dealer.businessName, disabled: true }];
-                config['assign_to_id'] = [
-                    {
-                        value: this.dealers.filter((dealer) => dealer.dealerId === feed.dealerId),
-                        disabled: true,
-                    },
-                ];
-            }
+            return this.getEditingFormConfig(baseConfig);
         }
 
-        this.new_feed_form = this._form.group(config);
-        this.matAutoFilter();
+        return baseConfig;
     }
 
-    /** Initialize Angular Material Autocomplete Component */
-    private matAutoFilter(): void {
+    private getEditingFormConfig(baseConfig: { [key: string]: any }): { [key: string]: any } {
+        const feed = this.fetched_feed;
+        return {
+            ...baseConfig,
+            feed_title: [feed ? feed.feedTitle : '', Validators.required],
+            description: [feed ? feed.description : ''],
+            feed_type: [
+                { value: feed && feed.feedType ? feed.feedType.feedTypeId : '', disabled: true },
+                Validators.required,
+            ],
+            assign_to: [{ value: null, disabled: true }],
+            assign_to_id: [{ value: null, disabled: true }],
+        };
+    }
+
+    private initializeAutocomplete(): void {
         this.filtered_options = this.new_feed_form.controls.assign_to.valueChanges.pipe(
             startWith(''),
             map((value) => this.filter(value)),
         );
+    }
+
+    public setAssignedTo(dealer: any): void {
+        this.dealerHasValue = false;
+        if (dealer) {
+            this.editHasValue = true;
+            this.selectedDealer = {
+                businessName: dealer.value,
+                dealerId: dealer.id,
+            };
+            this.new_feed_form.controls.assign_to.setValue(dealer.value);
+            this.new_feed_form.controls.assign_to_id.setValue(dealer.id);
+            this.dealerHasValue = true;
+        } else {
+            this.editHasValue = false;
+            this.new_feed_form.controls.assign_to.setValue('');
+            this.new_feed_form.controls.assign_to_id.setValue('');
+        }
+        this.new_feed_form.controls.assign_to.updateValueAndValidity();
+        this.new_feed_form.controls.assign_to_id.updateValueAndValidity();
+    }
+
+    goBack() {
+        this._location.back();
     }
 }
