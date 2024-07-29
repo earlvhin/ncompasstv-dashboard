28b0f09e0386e05c6956248e988e1f7f398d00be
epepftw <efgbt17@gmail.com>
2024-07-29 20:16:23 +0800

    fix(nsp-431): resolve comments and issues

--- src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
+++ src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
@@ -4,7 +4,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
 
-import { API_FEED_TYPES, API_GENERATED_FEED, GenerateSlideFeed } from 'src/app/global/models';
+import {
+    API_FEED_TYPES,
+    API_GENERATED_FEED,
+    GenerateSlideFeed,
+    UI_AUTOCOMPLETE_INITIAL_DATA,
+    USER_LOCALSTORAGE,
+} from 'src/app/global/models';
 import { AuthService, FeedService } from 'src/app/global/services';
 
 /**
@@ -22,24 +28,18 @@ export class FeedInfoComponent implements OnInit {
     @Input() feed_types: API_FEED_TYPES[];
     @Input() is_dealer: boolean = false;
     @Output() feed_info = new EventEmitter();
-    @Output() formChanges: EventEmitter<boolean> = new EventEmitter<boolean>();
+    @Output() form_changes: EventEmitter<boolean> = new EventEmitter<boolean>();
 
-    existing: any;
-    filtered_options: Observable<{ dealerId: string; businessName: string }[]>;
-    generated_feed: GenerateSlideFeed;
     hasUnsavedChanges: boolean = false;
     isDealer = this._auth.current_role === 'dealer' || this._auth.current_role === 'sub-dealer';
     isDisabled: boolean = false;
-    new_feed_form: FormGroup;
-    selected_dealer: string;
-    private formSubscription: Subscription;
-    currentUser: any;
+    newFeedForm: FormGroup;
+    currentUser: USER_LOCALSTORAGE;
     isEditingOrDealer: boolean = false;
-
-    // New properties for dealer-autocomplete
     hasLoadedDealers = false;
     isCurrentUserDealer = false;
-    selectedDealer: any[] = [];
+    selectedDealer: UI_AUTOCOMPLETE_INITIAL_DATA[] = [];
+    private formSubscription: Subscription;
 
     /**
      * Creates an instance of FeedInfoComponent.
@@ -64,10 +64,10 @@ export class FeedInfoComponent implements OnInit {
         this.loadDealers();
         this.prepareFeedInfoForm();
 
-        this.formSubscription = this.new_feed_form.valueChanges.subscribe((f) => {
+        this.formSubscription = this.newFeedForm.valueChanges.subscribe((f) => {
             this.hasUnsavedChanges =
                 f.feed_title || f.feed_type || f.description || (f.assign_to !== '' && !this.isDealer);
-            this.formChanges.emit(this.hasUnsavedChanges);
+            this.form_changes.emit(this.hasUnsavedChanges);
             this.updateHasUnsavedChanges(this.hasUnsavedChanges);
         });
     }
@@ -82,12 +82,12 @@ export class FeedInfoComponent implements OnInit {
 
     /**
      * Handles the beforeunload event.
-     * @param {any} $event - The beforeunload event.
+     * @param {any} beforeUnloadEvent - The beforeunload event.
      */
     @HostListener('window:beforeunload', ['$event'])
-    unloadNotification($event: any): void {
+    private unloadNotification(beforeUnloadEvent: BeforeUnloadEvent): void {
         if (this.hasUnsavedChanges) {
-            $event.returnValue = true;
+            beforeUnloadEvent.preventDefault();
         }
     }
 
@@ -105,45 +105,32 @@ export class FeedInfoComponent implements OnInit {
      * @param {string} data.id - The dealer ID.
      * @param {string} data.value - The dealer business name.
      */
-    dealerSelected(data: { id: string; value: string }) {
-        if (data) {
-            this.new_feed_form.patchValue({
-                assign_to: data.value,
-                assign_to_id: data.id,
-            });
-            this.selectedDealer = [data];
-        } else {
-            this.new_feed_form.patchValue({
-                assign_to: null,
-                assign_to_id: null,
-            });
-            this.selectedDealer = [];
-        }
+    public dealerSelected(data: UI_AUTOCOMPLETE_INITIAL_DATA | null): void {
+        this.newFeedForm.patchValue({
+            assign_to: data ? data.value : null,
+            assign_to_id: data ? data.id : null,
+        });
+
+        this.selectedDealer = data ? [data] : [];
+
         this.hasUnsavedChanges = true;
-        this.formChanges.emit(this.hasUnsavedChanges);
+        this.form_changes.emit(this.hasUnsavedChanges);
     }
 
     /**
      * Loads dealer information.
      * @private
      */
-    private loadDealers() {
+    private loadDealers(): void {
         this.hasLoadedDealers = true;
 
-        if (this.editing) {
-            if (this.fetched_feed.dealerId) {
-                this.selectedDealer = [
-                    {
-                        id: this.fetched_feed.dealerId,
-                        value: this.fetched_feed.dealer.businessName,
-                    },
-                ];
-            }
-        } else if (this.isCurrentUserDealer) {
+        const dealer = this.editing ? this.fetched_feed : this.currentUser.roleInfo;
+
+        if ((this.editing && dealer.dealerId) || this.isCurrentUserDealer) {
             this.selectedDealer = [
                 {
-                    id: this.currentUser.roleInfo.dealerId,
-                    value: this.currentUser.roleInfo.businessName,
+                    id: dealer.dealerId,
+                    value: dealer.businessName || dealer.dealer.businessName,
                 },
             ];
         }
@@ -176,14 +163,14 @@ export class FeedInfoComponent implements OnInit {
             config['assign_to_id'] = [{ value: this.currentUser.roleInfo.dealerId, disabled: true }];
         }
 
-        this.new_feed_form = this._form.group(config);
+        this.newFeedForm = this._form.group(config);
     }
 
     /**
      * Structures and emits the feed information.
      */
-    structureFeedInfo() {
-        const formValue = this.new_feed_form.value;
+    public structureFeedInfo(): void {
+        const formValue = this.newFeedForm.value;
         const feedInfo = {
             feed_title: formValue.feed_title,
             description: formValue.description,
