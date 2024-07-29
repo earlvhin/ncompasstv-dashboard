a168be82521f52f946c62eb9388216b1dff80818
efraim <efraimg@n-compass.biz>
2024-07-29 16:17:30 +0800

    fix(nsp-431): fixed newly created feed not showing

--- src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
+++ src/app/global/components_shared/feed_components/feed-info/feed-info.component.ts
@@ -7,6 +7,9 @@ import { map, startWith } from 'rxjs/operators';
 import { API_FEED_TYPES, API_GENERATED_FEED, GenerateSlideFeed } from 'src/app/global/models';
 import { AuthService, FeedService } from 'src/app/global/services';
 
+/**
+ * Component for managing feed information.
+ */
 @Component({
     selector: 'app-feed-info',
     templateUrl: './feed-info.component.html',
@@ -26,17 +29,39 @@ export class FeedInfoComponent implements OnInit {
     generated_feed: GenerateSlideFeed;
     hasUnsavedChanges: boolean = false;
     isDealer = this._auth.current_role === 'dealer' || this._auth.current_role === 'sub-dealer';
+    isDisabled: boolean = false;
     new_feed_form: FormGroup;
     selected_dealer: string;
     private formSubscription: Subscription;
+    currentUser: any;
+    isEditingOrDealer: boolean = false;
 
+    // New properties for dealer-autocomplete
+    hasLoadedDealers = false;
+    isCurrentUserDealer = false;
+    selectedDealer: any[] = [];
+
+    /**
+     * Creates an instance of FeedInfoComponent.
+     * @param {AuthService} _auth - The authentication service.
+     * @param {FormBuilder} _form - The form builder service.
+     * @param {FeedService} _feed - The feed service.
+     */
     constructor(
         private _auth: AuthService,
         private _form: FormBuilder,
         private _feed: FeedService,
-    ) {}
+    ) {
+        this.currentUser = this._auth.current_user_value;
+        this.isCurrentUserDealer = this._auth.current_role === 'dealer' || this._auth.current_role === 'sub-dealer';
+        this.isEditingOrDealer = this.editing || this.isCurrentUserDealer;
+    }
 
+    /**
+     * Initializes the component.
+     */
     ngOnInit() {
+        this.loadDealers();
         this.prepareFeedInfoForm();
 
         this.formSubscription = this.new_feed_form.valueChanges.subscribe((f) => {
@@ -47,11 +72,18 @@ export class FeedInfoComponent implements OnInit {
         });
     }
 
+    /**
+     * Cleans up the component.
+     */
     ngOnDestroy() {
         window.removeEventListener('beforeunload', this.unloadNotification);
         this.formSubscription.unsubscribe();
     }
 
+    /**
+     * Handles the beforeunload event.
+     * @param {any} $event - The beforeunload event.
+     */
     @HostListener('window:beforeunload', ['$event'])
     unloadNotification($event: any): void {
         if (this.hasUnsavedChanges) {
@@ -59,45 +91,68 @@ export class FeedInfoComponent implements OnInit {
         }
     }
 
+    /**
+     * Updates the unsaved changes status.
+     * @param {boolean} value - The new unsaved changes status.
+     */
     updateHasUnsavedChanges(value: boolean) {
         this._feed.setInputChanges(value);
     }
 
-    /** Structure Feed Information and Pass */
-    structureFeedInfo() {
-        if (!this.editing) {
-            this.feed_info.emit(this.new_feed_form.value);
-        } else {
-            /** Temp workaround (For some reason assign_to_id field value is not reflecting ) */
-            this.feed_info.emit({
-                feed_title: this.new_feed_form.controls.feed_title.value,
-                description: this.new_feed_form.controls.description.value,
-                feed_type: this.fetched_feed.feedType.feedTypeId,
-                assign_to_id: this.fetched_feed.dealerId,
+    /**
+     * Handles dealer selection.
+     * @param {Object} data - The selected dealer data.
+     * @param {string} data.id - The dealer ID.
+     * @param {string} data.value - The dealer business name.
+     */
+    dealerSelected(data: { id: string; value: string }) {
+        if (data) {
+            this.new_feed_form.patchValue({
+                assign_to: data.value,
+                assign_to_id: data.id,
             });
+            this.selectedDealer = [data];
+        } else {
+            this.new_feed_form.patchValue({
+                assign_to: null,
+                assign_to_id: null,
+            });
+            this.selectedDealer = [];
+        }
+        this.hasUnsavedChanges = true;
+        this.formChanges.emit(this.hasUnsavedChanges);
+    }
+
+    /**
+     * Loads dealer information.
+     * @private
+     */
+    private loadDealers() {
+        this.hasLoadedDealers = true;
+
+        if (this.editing) {
+            if (this.fetched_feed.dealerId) {
+                this.selectedDealer = [
+                    {
+                        id: this.fetched_feed.dealerId,
+                        value: this.fetched_feed.dealer.businessName,
+                    },
+                ];
+            }
+        } else if (this.isCurrentUserDealer) {
+            this.selectedDealer = [
+                {
+                    id: this.currentUser.roleInfo.dealerId,
+                    value: this.currentUser.roleInfo.businessName,
+                },
+            ];
         }
     }
 
     /**
-     * Filter Method for the Angular Material Autocomplete
-     * @param {string} value The entered phrase in the field
-     * @returns {dealerId: string, businessName: string} Array of filtered results
+     * Prepares the feed information form.
+     * @private
      */
-    private filter(value: string): { dealerId: string; businessName: string }[] {
-        const filter_value = value.toLowerCase();
-        const filtered_result = this.dealers
-            ? this.dealers.filter((i) => i.businessName.toLowerCase().includes(filter_value))
-            : [];
-
-        if (!this.is_dealer) {
-            this.selected_dealer = filtered_result[0] && value ? filtered_result[0].dealerId : null;
-            this.new_feed_form.controls.assign_to_id.setValue(this.selected_dealer);
-        }
-
-        return filtered_result;
-    }
-
-    /** Build Feed Information Form with fields of feed_title, description, assign_to */
     private prepareFeedInfoForm(): void {
         let config: { [key: string]: any };
         const feed = this.fetched_feed;
@@ -106,37 +161,36 @@ export class FeedInfoComponent implements OnInit {
             feed_title: ['', Validators.required],
             description: [''],
             feed_type: ['', Validators.required],
-            assign_to: [this.is_dealer ? this.dealers[0].businessName : ''],
-            assign_to_id: [this.is_dealer ? this.dealers[0].dealerId : ''],
+            assign_to: [''],
+            assign_to_id: [''],
         };
 
         if (this.editing) {
             config['feed_title'] = [feed.feedTitle, Validators.required];
             config['description'] = [feed.description];
             config['feed_type'] = [{ value: feed.feedType.feedTypeId, disabled: true }, Validators.required];
-            config['assign_to'] = [{ value: null, disabled: true }];
-            config['assign_to_id'] = [{ value: null, disabled: true }];
-
-            if (feed.dealerId) {
-                config['assign_to'] = [{ value: feed.dealer.businessName, disabled: true }];
-                config['assign_to_id'] = [
-                    {
-                        value: this.dealers.filter((dealer) => dealer.dealerId === feed.dealerId),
-                        disabled: true,
-                    },
-                ];
-            }
+            config['assign_to'] = [{ value: feed.dealer ? feed.dealer.businessName : '', disabled: true }];
+            config['assign_to_id'] = [{ value: feed.dealerId, disabled: true }];
+        } else if (this.isCurrentUserDealer) {
+            config['assign_to'] = [{ value: this.currentUser.roleInfo.businessName, disabled: true }];
+            config['assign_to_id'] = [{ value: this.currentUser.roleInfo.dealerId, disabled: true }];
         }
 
         this.new_feed_form = this._form.group(config);
-        this.matAutoFilter();
     }
 
-    /** Initialize Angular Material Autocomplete Component */
-    private matAutoFilter(): void {
-        this.filtered_options = this.new_feed_form.controls.assign_to.valueChanges.pipe(
-            startWith(''),
-            map((value) => this.filter(value)),
-        );
+    /**
+     * Structures and emits the feed information.
+     */
+    structureFeedInfo() {
+        const formValue = this.new_feed_form.value;
+        const feedInfo = {
+            feed_title: formValue.feed_title,
+            description: formValue.description,
+            feed_type: formValue.feed_type,
+            assign_to_id: this.isCurrentUserDealer ? this.currentUser.roleInfo.dealerId : formValue.assign_to_id,
+        };
+
+        this.feed_info.emit(feedInfo);
     }
 }
